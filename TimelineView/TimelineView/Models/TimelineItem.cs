e634403300026dea124bdb9eae1d5f706564deb8
Alexander Rojas <alexrrojas.b@gmail.com>
2019-01-22 16:07:02 -0600

    initial commit

---
+++ TimelineView/TimelineView/Models/TimelineItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TimelineView.Models
+{
+    public class TimelineItem
+    {
+        public string UserName { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
